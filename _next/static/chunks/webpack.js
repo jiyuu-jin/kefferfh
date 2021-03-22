360ec3f65d5b291c4ba2d07cc1638b7b7ed4c7b5
Zachary Pelkey <zpelkey@gmail.com>
2021-03-22 14:04:22 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c5930c5b1b5d70cc0723";
+/******/ 	var hotCurrentHash = "7ddf9d81b4763e341b5c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/prices":["static\u002Fchunks\u002Fpages\u002Fprices.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/immediate-burial":["static\u002Fchunks\u002Fpages\u002Fimmediate-burial.js"],"/prices":["static\u002Fchunks\u002Fpages\u002Fprices.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
